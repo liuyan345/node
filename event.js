23664fffe90a46ff5cf7ed8de474f6bfabdac4a5
liuyan <1260192349@qq.com>
2018-06-19 14:47:12 +0800

    test

--- event.js
+++ event.js
@@ -1,21 +1,25 @@
-var events = require("events");
-
+// 引入 events 模块
+var events = require('events');
+// 创建 eventEmitter 对象
 var eventEmitter = new events.EventEmitter();
 
-var connectHandler = function connected(){
-	console.log("链接成功。");
-
-	eventEmitter.emit("data_received");
+// 创建事件处理程序
+var connectHandler = function connected() {
+   console.log('连接成功。');
+  
+   // 触发 data_received 事件 
+   eventEmitter.emit('data_received');
 }
 
-eventEmitter.on("connection",connectHandler);
-
-eventEmitter.on("data_received",function(){
-	console.log("数据接收成功");
-
+// 绑定 connection 事件处理程序
+eventEmitter.on('connection', connectHandler);
+ 
+// 使用匿名函数绑定 data_received 事件
+eventEmitter.on('data_received', function(){
+   console.log('数据接收成功。');
 });
 
+// 触发 connection 事件 
+eventEmitter.emit('connection');
 
-eventEmitter.emit("connection");
-
-console.log("程序执行完毕");
+console.log("程序执行完毕。");
