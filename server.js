4e9e5e14b3339ed7397f9663f7f9f368ad421f1b
liuyan <1260192349@qq.com>
2018-06-20 15:19:02 +0800

    test

--- server.js
+++ server.js
@@ -7,27 +7,48 @@
 
 
 
-var http = require("http");
+// var http = require("http");
 
+// var url = require("url");
+
+// function start(route){
+// 	function onRequest(request,response) {
+// 		var pathname = url.parse(request.url).pathname;
+// 		console.log(pathname);
+		
+// 		route(pathname);
+
+// 		response.writeHead(200,{"Content-Type":"text/plain"});
+// 		response.write("Hello World "+pathname);
+// 		response.end();
+// 	}
+
+// 	http.createServer(onRequest).listen(8888);
+// 	console.log("Server has started.");
+// }
+
+// exports.start = start;
+
+
+
+var http = require("http");
+var fs = require("fs");
 var url = require("url");
 
-function start(route){
-	function onRequest(request,response) {
-		var pathname = url.parse(request.url).pathname;
-		console.log(pathname);
-		
-		route(pathname);
-
-		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write("Hello World "+pathname);
+http.createServer(function(require.response){
+	var pathname = url.parse(request.url).pathname;
+	console.log(pathname);
+	console.log(pathname.substr(1));
+	fs.readFile(pathname.substr(1),function(err,data){
+		if(err){
+			console.log(err);
+			response.writeHead(404,{'Content-Type':'text/html'});
+		}else{
+			response.writeHead(200,{'Content-Type':'text/html'});
+			response.write(data.toString());
+		}
 		response.end();
-	}
+	})
 
-	http.createServer(onRequest).listen(8888);
-	console.log("Server has started.");
-}
-
-exports.start = start;
-
-
+}).listen("8888");
 
