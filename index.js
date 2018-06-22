d13a03407d4adec54101adc8adc293066cd3d067
liuyan <1260192349@qq.com>
2018-06-22 16:20:06 +0800

    add

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require("express");
+var path = require('path');
+
+var ejs = require("ejs"); // 引入ejs模板
+
+var app = express();
+
+app.set("view engine","ejs");//设置模板类型
+app.set('views', path.join(__dirname, 'views')); // 设置模板路径
+
+app.use("/public",express.static("public"));// 设置静态文件目录
+
+//设置路由
+app.get("/",function(require,response){
+    response.send("this is home page");
+});
+
+app.get("/test:str",function(require,response){
+    response.send("this is test page "+require.params.str);
+});
+
+
+app.listen(8888);// 设置监听端口
