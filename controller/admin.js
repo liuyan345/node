333612b45a47fd3bef8c67c3a44e077f5f69b6f5
liuyan <1260192349@qq.com>
2018-06-27 17:24:19 +0800

    test

--- controller/admin.js
+++ controller/admin.js
@@ -1,6 +1,7 @@
 'use strict';
 var express = require('express');
 var admin = express.Router();
+var user = require("./admin/user");
 
 // 中间件处理程序
 admin.use(function(req,res,next){
@@ -14,9 +15,7 @@ admin.use(function(req,res,next){
 });
 
 //访问形式：http://node.qkllt.xin/base/read
-admin.get('/login', function(req, res) {
-    res.send("login")
-});
+admin.get('/login', user.login);
 
 admin.get('/plat', function(req, res) {
     res.send("plat")
---
+++ controller/admin/user.js
@@ -0,0 +1,7 @@
+var user = {};
+
+user.login = function (req,res,next){
+    res.send("this is login page");
+}
+
+model.exports = user;
